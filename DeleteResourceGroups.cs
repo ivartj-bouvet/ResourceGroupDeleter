f134d09e077c04d4ce6a682f094002bcb3b7a868
Ivar Trygve Jarlsby <ivar.jarlsby@bouvet.no>
2025-02-21 12:54:30 +0100

    Stuff.

--- DeleteResourceGroups.cs
+++ DeleteResourceGroups.cs
@@ -2,22 +2,29 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Azure;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
 
 namespace ResourceGroupDeleter
 {
-    public class DeleteResourceGroups
+    public class DeleteResourceGroups(IAzureClientFactory<ArmClient> factory, ILogger<DeleteResourceGroups> logger)
     {
-        private readonly ILogger<DeleteResourceGroups> _logger;
-
-        public DeleteResourceGroups(ILogger<DeleteResourceGroups> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ArmClient _armClient = factory.CreateClient(ArmClientName);
+        public const string ArmClientName = "ArmClient";
 
         [Function("DeleteResourceGroups")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var subscription = await _armClient.GetDefaultSubscriptionAsync();
+            var subscriptionId = subscription.Data.SubscriptionId;
+            var tasks = subscription.GetResourceGroups()
+                .Where(resourceGroup => !resourceGroup.GetManagementLocks().Any())
+                .Select(resourceGroup => resourceGroup.DeleteAsync(Azure.WaitUntil.Started))
+                .ToArray();
+            Task.WaitAll(tasks);
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+using ResourceGroupDeleter;
+using Microsoft.Azure.Functions.Worker;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+builder.Services.AddAzureClients(builder =>
+{
+    builder.AddArmClient("1a2cbe6d-efc5-4d59-8a28-f1124700a13e")
+        .WithName(DeleteResourceGroups.ArmClientName);
+});
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
