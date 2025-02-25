08000d1a64d5ba3cc63a332ae53c1799b18a77d5
Ivar Trygve Jarlsby <ivar.jarlsby@bouvet.no>
2025-02-25 09:14:32 +0100

    More logging; attempting to add parallelism.

--- DeleteResourceGroups.cs
+++ DeleteResourceGroups.cs
@@ -1,25 +1,37 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Azure;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Microsoft.Extensions.Logging;
 
 namespace ResourceGroupDeleter
 {
-    public class DeleteResourceGroups(IAzureClientFactory<ArmClient> factory)
+    public class DeleteResourceGroups(IAzureClientFactory<ArmClient> factory, ILogger<DeleteResourceGroups> logger)
     {
         private readonly ArmClient _armClient = factory.CreateClient(ArmClientName);
         public const string ArmClientName = "ArmClient";
 
         [Function("DeleteResourceGroups")]
-        public async Task Run([TimerTrigger("0 0 0 * * Sun")] TimerInfo timerInfo)
+        public void Run([TimerTrigger("0 0 0 * * Sun")] TimerInfo timerInfo)
         {
-            var subscription = await _armClient.GetDefaultSubscriptionAsync();
+            var subscription = _armClient.GetDefaultSubscription();
             var subscriptionId = subscription.Data.SubscriptionId;
-            var tasks = subscription.GetResourceGroups()
-                .Where(resourceGroup => !resourceGroup.GetManagementLocks().Any())
-                .Select(resourceGroup => resourceGroup.DeleteAsync(Azure.WaitUntil.Completed))
-                .ToArray();
-            await Task.WhenAll(tasks);
+            var resourceGroups = subscription.GetResourceGroups().ToArray();
+            logger.LogInformation("seeing the following resource groups: {}", resourceGroups.Select(resourceGroup => resourceGroup.Data.Name).Aggregate((a, b) => $"{a}, {b}"));
+            resourceGroups.AsParallel()
+                .ForAll(resourceGroup =>
+                {
+                    if (resourceGroup.GetManagementLocks().Any())
+                    {
+                        logger.LogInformation("resource group {} has one or more locks; not deleting", resourceGroup.Data.Name);
+                        return;
+                    }
+                    else
+                    {
+                        logger.LogInformation("resource group {} has no locks; deleting", resourceGroup.Data.Name);
+                        Task.Run(() => resourceGroup.Delete(Azure.WaitUntil.Completed));
+                    }
+                });
         }
     }
 }
